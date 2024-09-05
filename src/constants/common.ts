3b0a3622cda7eb969e7e8dc4e90d02544a1831c8
fushidane <danecosicol117@gmail.com>
2024-09-05 09:13:37 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,16 @@
+export const NAME = 'Dane P. Cosicol';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Enthusiastic college student with a foundational understanding of software development. Currently building skills in programming, debugging, and creating basic applications, with a desire to learn and grow in the field.';
+export const ABOUT_TEXT_2 =
+  'Gaining experience in web development, with a focus on learning both frontend and backend aspects. Eager to collaborate and contribute to projects while continuing to develop technical skills.';
+export const ABOUT_TEXT_3 =
+  'A beginner in web development, exploring various programming languages, frameworks, and databases. Committed to building complete web applications and improving proficiency step by step.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  '4th-year college student with a basic understanding of web development. Familiar with some modern JavaScript tools and frameworks, and actively working to improve both front-end and back-end skills.';
+export const RESUME_TEXT_2 =
+  '4th-year college student with introductory experience in web development, currently learning to use modern JavaScript tools and frameworks.';
+
